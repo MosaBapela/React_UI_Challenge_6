7a5e7a10a6adff24c7d3711e2711d546a6eaf2e4
MosaBapela <charitybapela2003@gmail.com>
2025-08-18 01:53:39 +0200

    Added app.css import

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { FirstContainer } from './components/FirstContainer/FirstContainer';
 import { SecondContainer } from './components/SecondContainer/SecondContainer';
 import { ThirdContainer } from './components/ThirdContainer/ThirdContainer';
 import { FourthContainer } from './components/FourthContainer/FourthContainer';
-
+//Components Properly Put
 function App() {
   return (
     <div id="app-container">
