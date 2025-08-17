515d207b282092d5aa854e1e47e492e643199159
MosaBapela <charitybapela2003@gmail.com>
2025-08-18 01:47:56 +0200

    fixed problems

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,50 +1,65 @@
-import styles from './Footer.module.css'
-import berries from '../../assets/berries_in_bowl.jpg'
-import meat from '../../assets/steak.png'
-import apple from '../../assets/apple.png'
-import cup from '../../assets/paper-cup.png'
+import React from 'react';
+import { ContentContainer } from '../ContentContainer/ContentContainer';
+import { Text } from '../Text/Text';
+import './Footer.css';
+import berries from '../../assets/berries_in_bowl.jpg';
+import meat from '../../assets/steak.png';
+import apple from '../../assets/apple.png';
+import cup from '../../assets/paper-cup.png';
 
-
-export const Footer = () => {
+export const Footer: React.FC = () => {
   return (
-    <footer>  
-            <div className={styles.mainDivs}>
+    <footer className="footer">
+      <ContentContainer className="footer__content">
+        <div className="footer__main">
+          <div className="footer__hero-image">
+            <img src={berries} alt="Fresh berries in a bowl" />
+          </div>
 
-              <div className={styles.divs_1}>
-                <img className={styles.berry} src={berries} alt="berries in a bowl" />
-              </div>
-
-              <div className={styles.divs_2}>
-                <div  className={styles.divs_2_1}>
-                      <img src={meat} alt="fresh steak" />
-                      <h5>FRESH</h5>
-                      
-                </div>
-
-                <div  className={styles.divs_2_1}>
-                      <img src={apple} alt="fresh steak" />
-                      <h5>VITAMIN</h5>
-                      
-                </div>
-
-                <div  className={styles.divs_2_1}>
-                     <img src={cup} alt="fresh steak" />
-                      <h5>ORE ANIC</h5>
-                      
-                </div>
-              </div>
-
-              <div className={styles.divs_3}>
-                <h5>OPENING HOURS</h5>
-                <ul>
-                  <li>Monday 17.00 cm * 12</li>
-                  <li>Tuesday 13.60 cm * 12</li>
-                  <li>Wednesday 19.00 cm * 11</li>
-                  <li>Thursday 17:00 cm * 11</li>
-                </ul>
-              </div>
-
+          <div className="footer__features">
+            <div className="footer__feature">
+              <img src={meat} alt="Fresh ingredients" />
+              <Text variant="h5" className="footer__feature-title" color="white">
+                FRESH
+              </Text>
             </div>
+
+            <div className="footer__feature">
+              <img src={apple} alt="Vitamin rich" />
+              <Text variant="h5" className="footer__feature-title" color="white">
+                VITAMIN
+              </Text>
+            </div>
+
+            <div className="footer__feature">
+              <img src={cup} alt="Organic products" />
+              <Text variant="h5" className="footer__feature-title" color="white">
+                ORGANIC
+              </Text>
+            </div>
+          </div>
+
+          <div className="footer__hours">
+            <Text variant="h5" className="footer__hours-title" color="white">
+              OPENING HOURS
+            </Text>
+            <ul className="footer__hours-list">
+              <li>
+                <Text variant="small" color="white">Monday 17:00 - 24:00</Text>
+              </li>
+              <li>
+                <Text variant="small" color="white">Tuesday 13:00 - 24:00</Text>
+              </li>
+              <li>
+                <Text variant="small" color="white">Wednesday 19:00 - 23:00</Text>
+              </li>
+              <li>
+                <Text variant="small" color="white">Thursday 17:00 - 23:00</Text>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </ContentContainer>
     </footer>
-  )
-}
+  );
+};
